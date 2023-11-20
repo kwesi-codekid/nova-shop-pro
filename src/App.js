7fd3581edaf1b528f77907d20d4cf661ef45f4ad
kwesi-codekid <kwasibordesjacob@gmail.com>
2023-11-20 05:37:22 +0000

    Boilerplate setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-violet-600">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-4xl text-blue-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
